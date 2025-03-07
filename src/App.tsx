d6a244ef6297c0c06c8990812b75c58de2fee70f
Matthew Miss <matt.w.miss@gmail.com>
2025-03-07 15:34:49 -0800

    Change Navbar to Header containing a nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import FlashcardsPage from './pages/FlashcardsPage';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 /**
  * Root component of the application.
  * 
- * - Currently renders the FlashcardsPage component.
+ * - Currently renders the Header and FlashcardsPage components.
  * - Can be expanded in the future to include navigation and routing.
  */
 function App() {
     return (
         /**
          * Main entry point of the application.
-         * Currently, it only renders FlashcardsPage.
          * 
          * If additional pages or features are added in the future (e.g., routing),
          * this file will serve as the top-level component.
          */
         <>
-            <Navbar />
+            <Header />
             <FlashcardsPage />
         </>
         
