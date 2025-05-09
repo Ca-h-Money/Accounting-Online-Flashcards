6ed23dbac46a4c037dac6a234c1ba1280d47d35d
Matt Miss <matt.w.miss@gmail.com>
2025-05-08 18:03:32 -0700

    Remove the top margin from the flashcard component since it was unnecessarily added

--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -25,7 +25,7 @@ function FlashCard({cardData, isFlipped, setIsFlipped, showTooltip, setShowToolt
     if(isFlipped) {
         return (
             <motion.button
-                className="mt-4 w-[280px] h-[280px] sm:w-[850px] sm:h-[450px] flex p-6 cursor-pointer rounded-lg shadow-sm
+                className="w-[280px] h-[280px] sm:w-[850px] sm:h-[450px] flex p-6 cursor-pointer rounded-lg shadow-sm
                         bg-white dark:bg-[#31A959]  dark:border-gray-700 dark:text-black
                         hover:bg-gray-100 dark:hover:bg-[#00A86B] items-center justify-center"
                 onClick={() => {
@@ -36,7 +36,8 @@ function FlashCard({cardData, isFlipped, setIsFlipped, showTooltip, setShowToolt
                 initial={{ rotateY: isInitialLoad ? 0 : 180 }}
                 animate={{ rotateY: 0 }}
                 transition={{ duration: 0.5 }}
-                style={{ transformStyle: "preserve-3d" }}>
+                style={{ transformStyle: "preserve-3d" }}
+            >
                 {showTooltip && 
                     <Tooltip 
                         className="absolute bottom-5 sm:bottom-10 right-3 sm:right-10"
@@ -65,17 +66,19 @@ function FlashCard({cardData, isFlipped, setIsFlipped, showTooltip, setShowToolt
     else if(cardData.back.length > 1){
         return (
             <motion.button
-                className="mt-4 w-[280px] h-[280px] sm:w-[850px] sm:h-[450px] flex p-6 cursor-pointer bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100
+                className="w-[280px] h-[280px] sm:w-[850px] sm:h-[450px] flex p-6 cursor-pointer bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100
                             dark:bg-gray-800 dark:border-gray-700 neon-border dark:hover:bg-gray-700 items-center justify-center"
                 onClick={() => setIsFlipped(true)}
                 initial={{ rotateY: 0 }}
                 animate={{ rotateY: 180 }}
                 transition={{ duration: 0.5 }}
-                style={{ transformStyle: "preserve-3d" }}>
+                style={{ transformStyle: "preserve-3d" }}
+            >
                 {/* Symmetrical T-shaped layout */}
                 <div
-                className="grid grid-cols-2 grid-rows-[20%_80%] w-full h-full relative backface-hidden"
-                style={{ transform: "rotateY(180deg)" }}>
+                    className="grid grid-cols-2 grid-rows-[20%_80%] w-full h-full relative backface-hidden"
+                    style={{ transform: "rotateY(180deg)" }}
+                >
                     {/* Vertical center line */}
                     <div className="absolute inset-y-0 left-1/2 w-px bg-gray-400 dark:bg-gray-500 transform -translate-x-1/2" />
                 
@@ -114,7 +117,7 @@ function FlashCard({cardData, isFlipped, setIsFlipped, showTooltip, setShowToolt
     else{
         return (
             <motion.button
-                className="mt-4 w-[280px] h-[280px] sm:w-[850px] sm:h-[450px] flex p-6 cursor-pointer bg-[#eed9c4] border border-gray-200 rounded-lg shadow-sm hover:bg-[#f7e7ce] dark:bg-gray-800
+                className="w-[280px] h-[280px] sm:w-[850px] sm:h-[450px] flex p-6 cursor-pointer bg-[#eed9c4] border border-gray-200 rounded-lg shadow-sm hover:bg-[#f7e7ce] dark:bg-gray-800
                             dark:border-gray-700 neon-border dark:hover:bg-gray-700 items-center justify-center"
                 onClick={() => setIsFlipped(true)}
                 initial={{ rotateY: 0 }}
@@ -124,9 +127,10 @@ function FlashCard({cardData, isFlipped, setIsFlipped, showTooltip, setShowToolt
                 transition={{ duration: 0.5,
                     ease: "easeInOut",
                  }}
-                style={{ transformStyle: "preserve-3d" }}>
-            {/*Back Content*/}
-            <h3 className="text-3xl lg:text-7xl font-bold tracking-tight text-gray-900 dark:text-white backface-hidden" style={{transform: "rotateY(180deg)"}}>{cardData.back[0]}</h3>
+                style={{ transformStyle: "preserve-3d" }}
+            >
+                {/*Back Content*/}
+                <h3 className="text-3xl lg:text-7xl font-bold tracking-tight text-gray-900 dark:text-white backface-hidden" style={{transform: "rotateY(180deg)"}}>{cardData.back[0]}</h3>
             </motion.button>)
     }
 }
