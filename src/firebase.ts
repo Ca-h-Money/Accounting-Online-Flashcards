81aee80a2d1b8b9b597470d4ba1688ad1a023673
garre <garrett.ballreich101@gmail.com>
2025-05-18 11:24:32 -0700

    switched test mode

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const testMode = false;
+const testMode = true;
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
